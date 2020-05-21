4a8288209b4b65df8fb1e8cd6cf076bf0e3279f3
AntipovDA <AntipovDA@mpei.ru>
2020-05-22 01:16:55 +0300

    3.2.7

--- main.cpp
+++ main.cpp
@@ -55,10 +55,15 @@ int main(int argc, char* argv[]) {
         CURL *curl = curl_easy_init();
         if(curl)
         {
-        CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            if (res != CURLE_OK)
+            {
+                cout << curl_easy_strerror(res) << endl;
+                exit(1);
+            }
+            curl_easy_cleanup(curl);
         }
     }
 
